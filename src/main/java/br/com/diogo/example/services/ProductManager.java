40ecc89928cf4cf03de779860fa0669778a691b5
Diogo Almeida <diogodma80@gmail.com>
2019-01-01 12:11:09 -0500

    added test to check if code exists before saving or altering the product

--- src/main/java/br/com/diogo/example/services/ProductManager.java
+++ src/main/java/br/com/diogo/example/services/ProductManager.java
@@ -39,24 +39,24 @@ public class ProductManager {
 		product.setName("Name " + code);
 		product.setPrice(10 * code);
 		product.setProductId(Integer.toString(code));
-		
+
 		return product;
 	}
-	
+
 	@GET
 	@Produces("application/json")
 	@Path("/{code}")
 	public Product getProduct(@PathParam("code") int code) {
-		
+
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		
+
 		Filter codeFilter = new FilterPredicate("Code", FilterOperator.EQUAL, code);
-		
+
 		Query query = new Query("Products").setFilter(codeFilter);
-		
+
 		Entity productEntity = datastore.prepare(query).asSingleEntity();
-		
-		if(productEntity != null) {
+
+		if (productEntity != null) {
 			Product product = entityToProduct(productEntity);
 			return product;
 		} else {
@@ -68,58 +68,62 @@ public class ProductManager {
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Product> getProducts() {
 		List<Product> products = new ArrayList<Product>();
-		
+
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		Query query = new Query("Products").addSort("Code", SortDirection.ASCENDING);
-		
+
 		List<Entity> productsEntity = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
-		
-		for(Entity productEntity:productsEntity) {
+
+		for (Entity productEntity : productsEntity) {
 			Product product = entityToProduct(productEntity);
 			products.add(product);
 		}
-		
+
 		return products;
 	}
-	
+
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Product saveProduct(Product product) {
 		
-		//gets an instance of Datastore service
+		// gets an instance of Datastore service
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		
-		//instantiates a key that will be used to create the product entity
-		Key productKey = KeyFactory.createKey("Products","productKey");
-		
-		//creates the new entity of type Products
-		Entity productEntity = new Entity("Products", productKey);
-		
-		//sets the "productEntity" entity properties
-		productToEntity(product, productEntity);
-		
-		//inserts the product
-		datastore.put(productEntity);
-		
-		product.setId(productEntity.getKey().getId());
-		return product;
+		if(!checkIfCodeExists(product)) {
+			// instantiates a key that will be used to create the product entity
+			Key productKey = KeyFactory.createKey("Products", "productKey");
+
+			// creates the new entity of type Products
+			Entity productEntity = new Entity("Products", productKey);
+
+			// sets the "productEntity" entity properties
+			productToEntity(product, productEntity);
+
+			// inserts the product
+			datastore.put(productEntity);
+
+			product.setId(productEntity.getKey().getId());
+			return product;
+		} else {
+			throw new WebApplicationException("There is already a product with the same code", Status.BAD_REQUEST);
+		}
 	}
-	
+
 	@DELETE
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{code}")
 	public Product deleteProduct(@PathParam("code") int code) {
-		
+
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		
+
 		Filter codeFilter = new FilterPredicate("Code", FilterOperator.EQUAL, code);
-		
+
 		Query query = new Query("Products").setFilter(codeFilter);
-		
+
 		Entity productEntity = datastore.prepare(query).asSingleEntity();
-		
-		if(productEntity != null) {
+
+		if (productEntity != null) {
 			datastore.delete(productEntity.getKey());
 			Product product = entityToProduct(productEntity);
 			return product;
@@ -127,31 +131,61 @@ public class ProductManager {
 			throw new WebApplicationException(Status.NOT_FOUND);
 		}
 	}
-	
+
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/{code}")
 	public Product alterProduct(@PathParam("code") int code, Product product) {
-		
+
+		if (product.getId() != 0) {
+
+			if (!checkIfCodeExists(product)) {
+				DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+				Filter codeFilter = new FilterPredicate("Code", FilterOperator.EQUAL, code);
+
+				Query query = new Query("Products").setFilter(codeFilter);
+
+				Entity productEntity = datastore.prepare(query).asSingleEntity();
+
+				if (productEntity != null) {
+					productToEntity(product, productEntity);
+					datastore.put(productEntity);
+					product.setId(productEntity.getKey().getId());
+					return product;
+				} else {
+					throw new WebApplicationException(Status.NOT_FOUND);
+				}
+			} else {
+				throw new WebApplicationException("There is a product with the same code", Status.BAD_REQUEST);
+			}
+
+		} else {
+			throw new WebApplicationException("The product ID must be informed", Status.BAD_REQUEST);
+		}
+	}
+
+	private boolean checkIfCodeExists(Product product) {
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		
-		Filter codeFilter = new FilterPredicate("Code", FilterOperator.EQUAL, code);
+		Filter codeFilter = new FilterPredicate("Code", FilterOperator.EQUAL, product.getCode());
 		
 		Query query = new Query("Products").setFilter(codeFilter);
 		
 		Entity productEntity = datastore.prepare(query).asSingleEntity();
 		
-		if(productEntity != null) {
-			productToEntity(product, productEntity);
-			datastore.put(productEntity);
-			product.setId(productEntity.getKey().getId());
-			return product;
+		if(productEntity == null) {
+			return false;
 		} else {
-			throw new WebApplicationException(Status.NOT_FOUND);
+			if(productEntity.getKey().getId() == product.getId()) {
+				return false;
+			} else {
+				return true;
+			}
 		}
 	}
-	
+
 	private void productToEntity(Product product, Entity productEntity) {
 		productEntity.setProperty("ProductId", product.getProductId());
 		productEntity.setProperty("Name", product.getName());
@@ -159,16 +193,16 @@ public class ProductManager {
 		productEntity.setProperty("Model", product.getModel());
 		productEntity.setProperty("Price", product.getPrice());
 	}
-	
+
 	private Product entityToProduct(Entity productEntity) {
 		Product product = new Product();
 		product.setId(productEntity.getKey().getId());
-		product.setProductId((String)productEntity.getProperty("ProductId"));
-		product.setName((String)productEntity.getProperty("Name"));
+		product.setProductId((String) productEntity.getProperty("ProductId"));
+		product.setName((String) productEntity.getProperty("Name"));
 		product.setCode(Integer.parseInt(productEntity.getProperty("Code").toString()));
-		product.setModel((String)productEntity.getProperty("Model"));
+		product.setModel((String) productEntity.getProperty("Model"));
 		product.setPrice(Float.parseFloat(productEntity.getProperty("Price").toString()));
 		return product;
-		
+
 	}
 }
