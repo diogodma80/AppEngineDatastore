d3703efb9d42831233ede7bb6185ae19112f1891
diogo <diogodma80@gmail.com>
2019-01-02 16:30:30 -0500

    implemented the authentication business logic on the filter method

--- src/main/java/br/com/diogo/example/authentication/AuthFilter.java
+++ src/main/java/br/com/diogo/example/authentication/AuthFilter.java
@@ -4,32 +4,52 @@ import java.io.IOException;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.DatatypeConverter;
 
 public class AuthFilter implements ContainerRequestFilter {
-	
+
 	private static final String ACCESS_UNAUTHORIZED = "You do not have permission to access this resource";
-	
-	
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
-		// TODO Auto-generated method stub
+
+		String auth = requestContext.getHeaderString("Authorization");
+
+		if (auth == null) {
+			requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity(ACCESS_UNAUTHORIZED).build());
+			return;
+		} else {
+			String[] loginPassword = decode(auth);
+			
+			if(loginPassword == null || loginPassword.length != 2) {
+				requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity(ACCESS_UNAUTHORIZED).build());
+				return;
+			} 
+
+			if(loginPassword[0].equals("Admin") && loginPassword[1].equals("Admin")){
+				return;
+			} else {
+				requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity(ACCESS_UNAUTHORIZED).build());
+			}
+		}
 
 	}
-	
+
 	private String[] decode(String auth) {
-		
+
 		auth = auth.replaceFirst("[B|b]asic ", "");
-		
-		//extracts the string after the value "Basic", decoding on the Base64 format returning a byte array
+
+		// extracts the string after the value "Basic", decoding on the Base64 format
+		// returning a byte array
 		byte[] decodedBytes = DatatypeConverter.parseBase64Binary(auth);
-		
-		if(decodedBytes == null || decodedBytes.length == 0) {
+
+		if (decodedBytes == null || decodedBytes.length == 0) {
 			return null;
 		}
-		
-		//if the decoded String is not null|0 an array containing two string is returned: "user:password"
+
+		// if the decoded String is not null|0 an array containing two string is
+		// returned: "user:password"
 		return new String(decodedBytes).split(":", 2);
 	}
 
